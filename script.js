90de60410e3590f8c66ee88a8f9c9e32795821ff
MonarxTv <monarxtv.uz@gmail.com>
2026-03-13 22:27:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -668,27 +668,3 @@ function prevShort() {
 
 // Dastlab render qilamiz
 renderShorts();
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js")
-  .then(() => console.log("Service Worker registered"))
-  .catch(err => console.log("Service Worker registration failed:", err));
-}
-let deferredPrompt;
-const installBtn = document.getElementById("installBtn");
-
-window.addEventListener("beforeinstallprompt", (e) => {
-  e.preventDefault();
-  deferredPrompt = e;
-  installBtn.style.display = "block";
-});
-
-installBtn.addEventListener("click", () => {
-  deferredPrompt.prompt();
-  deferredPrompt.userChoice.then((choiceResult) => {
-    if(choiceResult.outcome === 'accepted') {
-      console.log('User accepted the install prompt');
-    }
-    deferredPrompt = null;
-    installBtn.style.display = "none";
-  });
-});
